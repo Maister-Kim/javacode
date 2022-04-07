77cf5414804f8bf899a8cddaebf7a76cf1345a1e
Maister-Kim <98303150+Maister-Kim@users.noreply.github.com>
2022-04-07 15:58:03 +0900

    Update digit_Test.java

--- digit/digit_Test.java
+++ digit/digit_Test.java
@@ -8,16 +8,25 @@ public class digit_Test { // 수정중
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int year, month, days; // 년월일
+		int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int year = 0, month = 0, day, monthDays = 0; // 년월일
 		int ylength, mlength, dlength; // 년월일 자리수 확인용 변수
 		String str = ""; // 출력용 변수
 		
 		while(true)
 		{
-			try
-			{
-				System.out.println("연도 4자리 숫자 입력: ");
-				year = sc.nextInt();
+			do {
+				try	{
+					System.out.println("연도 4자리 숫자 입력: ");
+					year = sc.nextInt();
+				}
+				catch(InputMismatchException e) {
+					System.out.println("정수형만 입력");
+					sc.nextLine();
+				}
+				
+				if(year == 0)
+					System.exit(0);
 				ylength = (int)Math.log10(year);
 				// int형으로 변환해서 확인
 				// Math.log10() -> 숫자의 자리수 확인, 0부터 시작해서 자리수를 출력하므로 +1을 해줌
@@ -44,35 +53,54 @@ public class digit_Test { // 수정중
 						System.out.println("잘못입력");
 					}
 				}
-			}
-			catch(InputMismatchException e) {
-//				System.out.println(e.getMessage());
-				System.out.println("정수형만 입력");
-				sc.nextLine();
-			}
-			/*   		System.out.println("월 입력: ");
-			   		month = sc.nextInt();
-			   		if(month < 0 && month > 12)
-			   		{
-			   			System.out.println("잘못입력");
-			   		}
-			   		else
-			   		{
-			   			str += month + "월 ";
-			   		}
-			   		
-			   		System.out.println("일 입력: ");
-			   		days = sc.nextInt();
-			   		if(days < 0 && days > 31)
-			   		{
-			   			System.out.println("다시 입력");
-			   		}
-			   		else
-			   		{
-			   			str += days + "일";
-			   		}*/
-//			   		return;
+			}while(true);
+		
+	   		do {
+	   			try {
+	   				System.out.println("월 입력: ");
+	   				month = sc.nextInt();
+	   			}catch(InputMismatchException e) {
+		   			System.out.println("정수형만 입력");
+	   			}
+		   		if(month < 0 || month > 12)
+		   		{
+		   			System.out.println("잘못입력");
+		   		}
+		   		else
+		   		{
+		   			monthDays = days[month - 1];
+		   			str += month + "월 ";
+		   			break;
+		   		}
+	   		}while(true);
+	   		
+	   		do {
+		   		System.out.println("일 입력: ");
+		   		day = sc.nextInt();
+		   		
+	   			if(month == 2)
+		   		{
+		   			if((year % 4 == 0) && (year % 100 == 0) || (year % 400 == 0))
+		   			{
+		   				monthDays = 29; 
+		   			}
+		   			else
+		   			{
+		   				monthDays = 28;
+		   			}
+		   		}
+	   			
+	   			if(day < 0 || day > monthDays)
+		   			System.out.println("다시 입력");
+	   			else
+	   			{
+	   				str += days + "일";
+		   			break;
+	   			}
+	   		}while(true);
+	   		
+	   		System.out.println(str);
+	   		System.out.println();
 		}
-		System.out.println(str);
 	}
 }
